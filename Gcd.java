e4e189948b80f6b1611339d8bc80ffd323087402
KSurabhi <ksurabhi97@gmail.com>
2018-09-26 05:08:54 -0700

    GCD Using recursion

---
+++ Gcd.java
@@ -0,0 +1,28 @@
+package test;
+
+class Gcd
+{ 
+     
+    static int gcd(int a, int b) 
+    { 
+        
+        if (a == 0 || b == 0) 
+           return 0; 
+       
+        
+        if (a == b) 
+            return a; 
+       
+       
+        if (a > b) 
+            return gcd(a-b, b); 
+        return gcd(a, b-a); 
+    } 
+      
+   
+    public static void main(String[] args)  
+    { 
+        int a = 98, b = 56; 
+        System.out.println("GCD of " + a +" and " + b + " is " + gcd(a, b)); 
+    } 
+} 
